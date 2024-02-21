e3bec4e0f436e89e466c2b0e37622a9a9231a32e
AndrewIndeche <indecheandrew@outlook.com>
2024-02-21 15:18:24 +0300

    add search query states

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,8 +3,8 @@ import React, { useState, ChangeEvent, FormEvent } from 'react';
 interface SearchFormProps {
     onSearch: (query: string) => void;
 }
-
-const SearchBar: React.FC = () => {
+const SearchBar: React.FC = ({ onSearch}) => {
+    const [searchQuery, setSearchQuery] = useState<string>('');
     return()
 }
 
