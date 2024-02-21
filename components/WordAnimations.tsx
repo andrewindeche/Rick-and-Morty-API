0459d7e80d93df3e1e096ad5ef9439b2d6f23885
AndrewIndeche <indecheandrew@outlook.com>
2024-02-21 13:00:20 +0300

    add states for words

--- components/WordAnimations.tsx
+++ components/WordAnimations.tsx
@@ -3,6 +3,7 @@ import React, {useState,useEffect} from 'react';
 const words = ["HALLOO!!!", "WELCOME", "TO", "RICK", "AND", "MORTY", "SEARCH", "ENGINE!"]
 
 const WordAnimation: React.FC = () => {
+    const [visibleWords,setVisibleWords] = useState<string[]>([]);
     return(
 
     )
