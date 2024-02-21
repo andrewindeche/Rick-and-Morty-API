80e978f6e9e38e5f975a4234de6d9a817566c7c6
AndrewIndeche <indecheandrew@outlook.com>
2024-02-21 15:13:48 +0300

    add search form props

---
+++ components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+
+interface SearchFormProps {
+    onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC = () => {
+    return()
+}
+
+export default SearchBar;
