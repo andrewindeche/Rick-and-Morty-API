171abf5f6932f0f8f2c74af245fdc27ecf764204
AndrewIndeche <indecheandrew@outlook.com>
2024-02-21 12:56:44 +0300

    add Word Animation components

---
+++ components/WordAnimations.tsx
@@ -0,0 +1,11 @@
+import React, {useState,useEffect} from 'react';
+
+const words = ["HALLOO!!!", "WELCOME", "TO", "RICK", "AND", "MORTY", "SEARCH", "ENGINE!"]
+
+const WordAnimation: React.FC = () => {
+    return(
+
+    )
+};
+
+export default WordAnimation;
