1a4df1d383018e7e46c7ea6d789a30ed51339be2
AndrewIndeche <indecheandrew@outlook.com>
2024-02-22 12:58:42 +0300

    create search page fro routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import NavigationBar from 'components/NavigationBar';
-import WordAnimations from 'components/WordAnimations';
-import SearchBar from 'components/SearchBar';
-import ImagePosters from 'components/ImagePosters';
-import SearchResults from 'components/SearchResults';
-import LocationResults from 'components/LocationResults';
+import LandingPage from 'components/LandingPage';
+
 const Home: React.FC = () => {
   const handleSearch = (query: string) => {
     console.log('Searching for:', query);
@@ -12,8 +9,7 @@ const Home: React.FC = () => {
     return (
     <div>
     <NavigationBar />
-    <SearchBar onSearch={handleSearch} />
-    <SearchResults />
+    <LandingPage />
     </div>
     )
   };
