418c215d70c0f50ce97a7fae7c701cc844f8ba52
AndrewIndeche <indecheandrew@outlook.com>
2024-02-22 13:42:50 +0300

    add Layout component for common navbar

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchBar from './SearchBar';
+import SearchBar from 'components/SearchBar';
 const SearchResults: React.FC = () => {
   const handleSearch = (query: string) => {
     console.log('Searching for:', query);
