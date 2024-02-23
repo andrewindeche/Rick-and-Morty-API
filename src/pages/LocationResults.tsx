61bf502de1e3787ddf924c629e9de81e554f9f46
AndrewIndeche <indecheandrew@outlook.com>
2024-02-23 10:32:09 +0300

    add states and promises for characters in locations

--- src/pages/LocationResults.tsx
+++ src/pages/LocationResults.tsx
@@ -3,49 +3,51 @@ import SearchBar from 'components/SearchBar';
 import Layout from 'components/Layout';
 import axios from 'axios';
 const Location: React.FC = () => {
-  const [locationDetails, setLocationDetails] = useState<any>({});
-  const [residents, setResidents] = useState<any[]>([]);
+  const [locationResults, setLocationResults] = useState<any>({});
+  const [locationName, setLocationName] = useState<string | null>(null);
+  const [characters, setCharacters] = useState<any[]>([]);
 
-  
-  const handleSearch = (query: string) => {
-  console.log('Searching for:', query);
+  const handleSearch = async(query: string)  => {
+    try {
+      const response = await axios.get(`https://rickandmortyapi.com/api/location/?name=${encodeURIComponent(query)}`);
+      setLocationResults(response.data.results);
+      setLocationName(query);
+
+      const characterPromises = response.data.results[0]?.residents.map((resident: string) => axios.get(resident));
+      const characterResponses = await Promise.all(characterPromises);
+      const characterData = characterResponses.map((characterResponse: any) => characterResponse.data);
+      setCharacters(characterData);
+    } catch (error) {
+      console.error('Error fetching location data:', error);
+      setLocationResults([]);
+      setLocationName(null);
+      setCharacters([]);
+    }
   };
     return(
       <>
       <Layout>
       <SearchBar onSearch={handleSearch} />
         <div className='LocationResults'>
+        {locationName && (
           <div className="header">
-        <div className="LocationHeader">LOCATION: <span className='headername'>EARTH</span></div>
-        <div className="ResidentsHeader">RESIDENTS: <span className='headername'>826</span></div>
+        <div className="LocationHeader">LOCATION: <span className='headername'>{locationName}</span></div>
+        <div className="ResidentsHeader">RESIDENTS: <span className='headername'>{locationResults.length}</span></div>
           </div>
-      <div className="results">5 RESULT FOR: <span className='PlanetName'>EARTH</span></div>
+          )}
+          {locationResults.length > 0 ? (
+        <div className="results">{locationResults.length} RESULT FOR: <span className='PlanetName'>{locationName}</span></div>
+        ) : (
+          <div className="results">No results found for: <span className='PlanetName'>{locationName}</span></div>
+        )}
       <div className="cards">
-        <div className="card">
+      {locationResults.map((location, index) => (
+        <div className="card" key={index}>
           <img src="images/centaur.jpeg" alt="Centaur" />
           <div className="name">CENTAUR</div>
           <div className="description">ALIVE - MYTHOLOGICAL CREATURE</div>
         </div>
-        <div className="card">
-          <img src="images/randy.jpeg" alt="Randy Dicknose" />
-          <div className="name">RANDY DICKNOSE</div>
-          <div className="description">ALIVE - INTERDIMENSIONAL CABLE</div>
-        </div>
-        <div className="card">
-          <img src="images/morphizer.jpeg" alt="Morphizer-XE Customer Support" />
-          <div className="name">MORPHIZER-XE CUSTOMER SUPPORT</div>
-          <div className="description">ALIVE - ALIEN</div>
-        </div>
-        <div className="card">
-          <img src="images/facistshrimpss.jpeg" alt="Facist Shrimp $$" />
-          <div className="name">FACIST SHRIMP $$</div>
-          <div className="description">ALIVE - ANIMAL</div>
-        </div>
-        <div className="card">
-          <img src="images/toocute.jpeg" alt="TooCuteToMurderSummer $$" />
-          <div className="name">TOO CUTE TO MURDER SUMMER $$</div>
-          <div className="description">DEAD - ROBOT</div>
-        </div>
+        ))}
       </div>
         </div>
         </Layout>
