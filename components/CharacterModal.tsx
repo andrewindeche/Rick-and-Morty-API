f4bc87180268980487d93b08faee9b7b26bc0c7d
AndrewIndeche <indecheandrew@outlook.com>
2024-02-24 20:43:12 +0300

    add modal component details for rendering

---
+++ components/CharacterModal.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+interface CharacterModalProps {
+    character: {
+      image: string;
+      location: { name: string };
+      origin: { name: string };
+      status: string;
+      species: string;
+      name: string;
+    };
+    onClose: () => void;
+  }
+
+const CharacterModal: React.FC<CharacterModalProps> = ({ character, onClose }) => {
+  const [note, setNote] = useState('');
+  const [postedNotes, setPostedNotes] = useState<string[]>([]);
+
+  const handlePostNote = () => {
+    if (note.trim() !== '') {
+      setPostedNotes((prevNotes) => [...prevNotes, note]);
+      setNote('');
+    }
+  };
+    return(
+        <div className="character-modal">
+        <div className="modal-content">
+        {character && character.image && ( 
+          <img src={character.image} alt={character.name} />
+          )}
+          <div className="details">
+          {character && ( 
+            <>
+            <div className="detail-row">
+              <span className="label">Last Known Location:</span>
+              <span className="value">{character.location.name}</span>
+            </div>
+            <div className="detail-row">
+              <span className="label">First Seen In:</span>
+              <span className="value">{character.origin.name}</span>
+            </div>
+            <div className="detail-row">
+              <span className="label">Alive:</span>
+              <span className="value">{character.status === 'Alive' ? 'Yes' : 'No'}</span>
+            </div>
+            <div className="detail-row">
+              <span className="label">Species:</span>
+              <span className="value">{character.species}</span>
+            </div>
+            </>
+            )}
+          </div>
+          <div className="notes-section">
+            <textarea
+              placeholder="Add notes about the character..."
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
+            />
+            <button onClick={handlePostNote}>Post Note</button>
+            <div className="posted-notes">
+              {postedNotes.map((postedNote, index) => (
+                <div key={index} className="posted-note">
+                  {postedNote}
+                </div>
+              ))}
+            </div>
+          </div>
+          <button className="close-button" onClick={onClose}>
+            Close
+          </button>
+        </div>
+      </div>
+    )
+}
+
+export default CharacterModal;
